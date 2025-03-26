52faa2d0b2c6be432d6de4b74677d740334b58d6
marcapedoh <adralabel@gmail.com>
2025-03-26 01:55:16 +0100

    update project

--- couche_application.rkt
+++ couche_application.rkt
@@ -3,40 +3,38 @@
 (provide simplification)
 
 
-(define simp* (lambda (exp)                                                                                    ;simp* : expression ----> expression
-              
+(define simp* (lambda (exp)   
           (cond
-              ((eq? (simplification (operande_test_1 exp)) 1)             (simplification (operande_test_2 exp)))          ;operande1 = 1                                                                             
-              ((eq? (simplification (operande_test_2 exp)) 1)             (simplification (operande_test_1 exp)))          ;operande2 = 1
+              ((eq? (simplification (operande1 exp)) 1)             (simplification (operande2 exp)))          ;operande1 = 1                                                                             
+              ((eq? (simplification (operande2 exp)) 1)             (simplification (operande1 exp)))          ;operande2 = 1
 
-              ((or (eq? (simplification (operande_test_1 exp))  0)                                                   ;l'un des operandes est null
-                   (eq? (simplification (operande_test_2 exp)) 0))       0)
+              ((or (eq? (simplification (operande1 exp))  0)                                                   ;l'un des operandes est null
+                   (eq? (simplification (operande2 exp)) 0))       0)
         
-             ((and (constante? (simplification (operande_test_1 exp)))
-                   (constante? (simplification (operande_test_2 exp))))   (* (simplification (operande_test_1 exp))
-                                                                    (simplification (operande_test_2 exp))))        ;les deux operandes sont des constants
+             ((and (constante? (simplification (operande1 exp)))
+                   (constante? (simplification (operande2 exp))))   (* (simplification (operande1 exp))
+                                                                             (simplification (operande2 exp))))        ;les deux operandes sont des constants
 
-            (#t                                                     (list (simplification (operande_test_1 exp))    ;au moins un des operandes est une liste
-                                                                          (operateur_as_symbol exp)
-                                                                          (simplification (operande_test_2 exp))))
+            (#t                                                           (list (simplification (operande1 exp))    ;au moins un des operandes est une liste
+                                                                                (operateur exp)
+                                                                                (simplification (operande2 exp))))
             )))
 
 
 
 
-(define simp+ (lambda (exp)                                                                                     ;simp+: expression ----> expression
-              
+(define simp+ (lambda (exp)     
           (cond
-            ((eq? (simplification (operande_test_1 exp)) 0)              (simplification (operande_test_2 exp)))            ;operande1 = 0
-            ((eq? (simplification (operande_test_2 exp)) 0)              (simplification (operande_test_1 exp)))            ;operande2 = 0
+            ((eq? (simplification (operande1 exp)) 0)              (simplification (operande2 exp)))            ;operande1 = 0
+            ((eq? (simplification (operande2 exp)) 0)              (simplification (operande1 exp)))            ;operande2 = 0
 
-            ((and (constante? (simplification (operande_test_1 exp)))
-                  (constante? (simplification (operande_test_2 exp))))   (+ (simplification (operande_test_1 exp))
-                                                                      (simplification (operande_test_2 exp))))         ;les deux operandes sont des constants
+            ((and (constante? (simplification (operande1 exp)))
+                  (constante? (simplification (operande2 exp))))   (+ (simplification (operande1 exp))
+                                                                            (simplification (operande2 exp))))         ;les deux operandes sont des constants
 
-            (#t                                                    (list (simplification (operande1 exp))        ;au moins un des operandes est une liste
-                                                                         (operateur_as_symbol exp)
-                                                                         (simplification (operande2 exp))))
+            (#t                                                           (list (simplification (operande1 exp))        ;au moins un des operandes est une liste
+                                                                                (operateur exp)
+                                                                                (simplification (operande2 exp))))
           )))
 
 
@@ -45,16 +43,11 @@
     ((or (identificateur? exp)
          (constante? exp))            
      exp)
-    
-    ((surparenthese? exp) 
-     
-     (if (pair? (car exp))  ; Vérifie que (car exp) est bien une liste avant de l'appeler récursivement
-         (simplification (car exp))
-         (car exp)))  ; Si ce n’est pas une liste, on le retourne tel quel
-    
-    ((eq? '+ (operateur_as_symbol exp)) (simp+ exp))                                 
-    ((eq? '* (operateur_as_symbol exp)) (simp* exp))                                
+    ((surparenthese? exp) (simplification (car exp)))
+    ((eq? '+ (operateur exp)) (simp+ exp))                                 
+    ((eq? '* (operateur exp)) (simp* exp))                                
   )))
+
 
 
 (simplification '(((x) + (5 * y)) + (z)))
@@ -62,3 +55,4 @@
 (simplification '((* 8 0) + (+ 8 4)))
 (simplification '(4 + (2 * 3)))
 (simplification '((12 + (1 * 5)) + (3 + 0)))
+(simplification '((((1 * x) + (0 * y)) + (z))))
--- couche_metier.rkt
+++ couche_metier.rkt
@@ -1,63 +1,61 @@
 #lang scheme
 
-(provide operateur operande1 operande2 operateur_as_symbol operande_test_1 operande_test_2  constructeur constante? identificateur? surparenthese?)
+(provide   operateur operande1 operande2 constructeur constante? identificateur? surparenthese?)
+
+#|
+    une expression arthmétique a trois forme d'écriture
+   1- préfixé: (+ x y)
+   2- infixé:  (x + y)
+   3- post-fixé: (x y +)
 
 
+|#
 
-(define last ;cette fonction retourne le dernier élément de la liste s'il existe sinon on retourne la liste vide
+
+(define dernier_liste_element ;cette fonction retourne le dernier élément de la liste s'il existe
   (lambda (lst)
     (if (null? (cdr lst))
         (car lst)
-        (last (cdr lst))
+        (dernier_liste_element (cdr lst))
      )
    )
 )
 
-(define operateur_as_symbol (lambda (exp)
+(define operateur (lambda (exp);cette fonction prend une fonction en parametre et retourne l'operateur de cette expression
                               (cond
                                 ((symbol? (car exp)) (car exp))
                                 ((symbol? (cadr exp)) (cadr exp))
-                                (else (last exp))
+                                (else (dernier_liste_element exp))
                                 )
                              )
 )
-                                           
-(define operateur (lambda (exp)              ;la fonction operateur:expression -----> operateur
-                  (car (cdr exp))            ;elle renvoie l'operateur principale d'une expression arithmétique
-        ))
 
 
 
                                                                         
-(define operande_test_1(lambda(exp) ;cette fonction retourne le premier opérateur de l'expression
+(define operande1(lambda(exp) ;cette fonction retourne la premiere operande de l'expression
+                         
                    (cond
                      ((symbol? (car exp)) (cadr exp))
                      ((symbol? (cadr exp)) (car exp))
-                     ((symbol? (last exp)) (car exp))
-                     
+                     ((symbol? (dernier_liste_element exp)) (car exp))
+                     (else exp)
                     )
                  )
-)                                                                   
-(define operande1 (lambda (exp)              ;la fonction operande1 : expression ----> expression
-                 (car exp)                   ;elle renvoie la première opérande d'une expression arithmétique.
-        ))
+)
 
 
 
                                                                         
-(define operande_test_2(lambda(exp); cette fonction retour le deuxieme opérateur de l'équation
+(define operande2(lambda(exp); cette fonction retour la deuxieme operande de l'expression
                    (cond
                      ((symbol? (car exp)) (caddr exp))
                      ((symbol? (cadr exp)) (caddr exp))
-                     ((symbol? (last exp)) (cadr exp))
+                     ((symbol? (dernier_liste_element exp)) (cadr exp))
+                     (else exp)
                     )
                   )
  )                                                                   
-(define operande2 (lambda (exp)              ;la fonction opereande2 : expression ----> expression
-                 (car (cdr (cdr exp)))       ;elle renvoie la deuxième opérande d'une expression arithmétique.
-        ))
-
-
                                                                         
                                                                         
 (define constante? (lambda (exp)                 ;la fonction const?: expression ----> booléen
@@ -73,16 +71,17 @@
                                                                         
                                                                         
 (define surparenthese? (lambda (exp)          ;la fonction : expression -----> booléen
-                  (= (length exp) 1)
+                         (and (list? exp) (= (length exp) 1))
                          ))
 
-(define constructeur (lambda (exp1 operateur exp2)
+
+(define constructeur (lambda (exp1 operateur exp2) ;cette fonction prend en parametre deux operandes et un operateur et créer une expression arthmétique
                        (list exp1 operateur exp2)
                        ))
                    
-(operateur_as_symbol '((X) + (1 * y)))
-(operateur_as_symbol '(3 / 2))
-(operateur_as_symbol '(5 9 *))
+(operateur '((X) + (1 * y)))
+(operateur '(3 / 2))
+(operateur '(5 9 *))
 (constante? '(5))
 (constante? 9)
 (identificateur? 45)
@@ -91,18 +90,9 @@
 (constructeur '+ 'x 7812)
 (constructeur 'x '+ 'y)
 (constructeur 'x 'z '*)
-;(operateur '(+ 1 6))
-;(operateur '(3 / 2))
-;(operateur '(5 9 *))
-(operande_test_1 '(+ (3 / 2) x))
-(operande_test_1 '(+ X 6))
-(operande_test_1 '(5 9 *))
-;(operande1 '(3 / 2))
-;(operande1 '(+ X 6))
-;(operande1 '(5 9 *))
-(operande_test_2 '(+ 1 Y))
-(operande_test_2 '(8 / 2))
-(operande_test_2 '(5 8 *))
+(operande1 '(+ (3 / 2) x))
+(operande1 '(+ X 6))
+(operande1 '(5 9 *))
 (operande2 '(+ 1 Y))
 (operande2 '(8 / 2))
 (operande2 '(5 8 *))
